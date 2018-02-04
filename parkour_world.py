c58426eed138d7df961a54e43f1e408a90745301
ArkUnit <unitark@gmail.com>
2018-02-04 18:32:50 +0100

    Added player controles code, V0.9

--- parkour_world.py
+++ parkour_world.py
@@ -38,30 +38,53 @@ COLORS = [
 	pygame.Color(120,120,120), #GREY
 ]
 
-niveau = Niveau(LEVELS[0],WORLD_IMG[0],WORLD_IMG[1],WORLD_IMG[2])
-physics = Physics()
+niveau = Niveau(LEVELS[0],WORLD_IMG)
+joueur = Joueur(420,250,PLAYER_IMG[0],0.2)
 
-All_object = [Joueur(400,250,PLAYER_IMG[0])]
 
 background_color = pygame.Surface((xWmax,yWmax))
-world_surface = pygame.Surface((xWmax,yWmax))
 background_color.fill(COLORS[5])
 
 while_bool = True
 
 niveau.read()
-world_surface.blit(background_color,(0,0))
-niveau.afficher(world_surface)
+niveau.afficher()
 
 while while_bool:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+		if event.type == KEYDOWN:
+			if event.key == K_RIGHT:
+				joueur.xs = 2
+			if event.key == K_LEFT:
+				joueur.xs = -2
+			if event.key == K_UP:
+				if joueur.have_gravity:
+					joueur.jump()
+				else:
+					joueur.ys = -2
+			if event.key == K_DOWN:
+				if joueur.have_gravity == False:
+					joueur.ys = 2
+					
+		elif event.type == KEYUP:
+			if event.key == K_RIGHT or event.key == K_LEFT:
+				joueur.xs = 0
+			if event.key == K_UP or event.key == K_DOWN:
+				if joueur.have_gravity == False:
+					joueur.ys = 0
 	
-	physics.onGravity(All_object)
-	All_object[0].update()
-	window.blit(world_surface,(0,0))
-	All_object[0].draw(window)
+	
+	joueur.update(niveau.print_block)
+	
+	window.blit(background_color,(0,0))
+	niveau.update(joueur)
+	for b in niveau.print_block:
+		window.blit(b.image,b.rect)
+	
+	joueur.draw(window)
+	
 	pygame.display.update()
 	clock.tick(60)
--- parkour_world_class.py
+++ parkour_world_class.py
@@ -3,66 +3,104 @@ from pygame.locals import *
 
 
 pygame.init()
-
-
-class Physics():
-	
-	def __init__(self):
-		self.velocity = 0.2
-		self.IsGravity = True
-	
-	def onGravity(self,l):
-		
-		if self.IsGravity:
-			for obj in l:
-				if obj.have_gravity:
-					obj.ys += self.velocity
-		else:
-			print("No Gravity !")
 				
 
 
 class Joueur(pygame.sprite.Sprite):
 	
-	def __init__(self,x ,y,image):
+	def __init__(self,x ,y,image,velocity):
 		self.x = x
 		self.y = y
 		self.xs, self.ys = 0,0
 		
 		self.have_gravity = True
+		self.velocity = velocity
+		
+		self.isGrounded = False
+		self.isJumping = False
+		
+		self.try_collide = []
 		
 		self.image = image
-		
-		self.update()
-	
-	def update(self):
-		
-		self.x += self.xs
-		self.y += self.ys
 		
 		self.rect = (int(self.x),int(self.y),self.image.get_width(),self.image.get_height())
 		self.mask = pygame.mask.from_surface(self.image)
 	
+	def update(self,b_l):
+		self.rect = (int(self.x),int(self.y),self.image.get_width(),self.image.get_height())
+		
+		
+		for b in b_l:
+			if b.x < self.x + 21 and b.x > self.x - 21:
+				if b.y < self.y + 21 and b.y > self.y - 21:
+					if b not in self.try_collide:
+						self.try_collide.append(b)
+				else:
+					if b in self.try_collide:
+						self.try_collide.remove(b)
+			else:
+				if b in self.try_collide:
+					self.try_collide.remove(b)
+		
+		if len(self.try_collide) != 0:
+			for b in self.try_collide:
+				if pygame.sprite.collide_mask(self,b) == False:
+					self.isGrounded = False
+				elif pygame.sprite.collide_mask(self,b):
+					self.isGrounded = True
+					self.y = b.y - 20
+		else:
+			self.isGrounded = False
+			
+		print(len(self.try_collide))
+		
+		if self.isGrounded == False:
+			if self.have_gravity:
+				self.ys += self.velocity
+		
+		if self.isGrounded:
+			self.y -= self.ys
+			self.ys = 0
+			
+		self.y += self.ys
+		self.x += self.xs
+		
+		self.mask = pygame.mask.from_surface(self.image)
+	
 	def jump(self):
-		self.ys = -5
+		if self.isGrounded:
+			self.isJumping = True
+			self.isGrounded = False
+			self.ys = -5
+			self.y -= 5
 	
 	def draw(self,surface):
 		surface.blit(self.image,self.rect)
+		
 
+
+class Block(pygame.sprite.Sprite):
+	
+	def __init__(self,x,y,img):
+		self.x,self.y = x,y
+		self.image = img
+		self.mask = pygame.mask.from_surface(self.image)
+		self.rect = (int(x),int(y),self.image.get_width(),self.image.get_height())
 
 
 class Niveau:
 	"""Classe permettant de créer un niveau"""
 	
-	def __init__(self, fichier, h, t, p):
+	def __init__(self, fichier,tex):
 		self.fichier = fichier
 		self.structure = 0
 		
-		self.herbe = h
-		self.terre = t
-		self.pierre = p
+		self.textures = tex
 		
 		self.taille_sprite = 20
+		
+		self.world_block = []
+		self.print_block = []
 		
 	def read(self):
 		""" Méthode permettant de lire 
@@ -83,7 +121,7 @@ class Niveau:
 			self.structure = structure_niveau
 	
 	
-	def afficher(self, window):
+	def afficher(self):
 		"""Méthode permettant d'afficher le niveau en fonction 
 		de la liste de structure renvoyée par generer()
 		"""
@@ -97,10 +135,25 @@ class Niveau:
 				x = num_case * self.taille_sprite
 				y = num_ligne * self.taille_sprite
 				if sprite == '1':		   
-					window.blit(self.terre, (x,y))
+					self.world_block.append(Block(x,y,self.textures[0]))
 				if sprite == '2':
-					window.blit(self.pierre, (x,y))
+					self.world_block.append(Block(x,y,self.textures[1]))
 				if sprite == '3':
-					window.blit(self.herbe, (x,y))
+					self.world_block.append(Block(x,y,self.textures[2]))
 				num_case += 1
 			num_ligne += 1
+	
+	def update(self,joueur):
+		
+		for b in self.world_block:
+			if b.x < joueur.x + 700 and b.x > joueur.x - 700:
+				if b.y < joueur.y + 500 and b.y > joueur.y - 500:
+					if b not in self.print_block:
+						self.print_block.append(b)
+				else:
+					if b in self.print_block:
+						self.print_block.remove(b)
+			else:
+				if b in self.print_block:
+					self.print_block.remove(b)
+			
